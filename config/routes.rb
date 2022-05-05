d758ac2daf11481d3cff10e8dc8cfefa4921b010
Peyton <pekellogg@hotmail.com>
2022-05-05 13:25:13 -0700

    commit tree d0416c34543a7cdba3fa03ced6f0e3da613ade82

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.create(name: "'03 Bonnie & Clyde ft. Beyoncé Knowles", genre_id: 1, artist_id: 2)
+Song.create(name: "Dangerously in Love", genre_id: 2, artist_id: 1)
